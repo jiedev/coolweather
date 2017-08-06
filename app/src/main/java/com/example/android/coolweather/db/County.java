3a06d77038a4046e19fe47ba12cb9552dd9ab643
Jie <jie@gmail.com>
2017-08-06 16:42:29 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/android/coolweather/db/County.java
@@ -0,0 +1,49 @@
+package com.example.android.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/8/6.
+ * 建立县，属性id，CountyName，WeatherId，CityId
+ * getter and setter方法
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
